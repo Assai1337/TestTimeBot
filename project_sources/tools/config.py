fed9cca1c064d3994102daa4daa9fdd66d8c811f
Вадим <vikvad25@gmail.com>
2024-12-06 20:58:19 +0300

    add sources

--- project_sources/tools/config.py
+++ project_sources/tools/config.py
@@ -1,6 +1,5 @@
-import env
 
-BOT_TOKEN = env.BOT_TOKEN
-DATABASE_URL = env.DATABASE_URL
-ADMIN_CHAT_ID = env.ADMIN_CHAT_ID
-ADMIN_USERNAME = env.ADMIN_USERNAME
+BOT_TOKEN = "7897071464:AAFXU9nEu4VrvwsIDlSqYde0ZSIwFrMdX9o"
+DATABASE_URL = "postgresql+asyncpg://postgres:pisklov228@localhost:5432/testing_bot_db"
+ADMIN_CHAT_ID = "866419155"
+ADMIN_USERNAME = "tokyospliffs"
